70518c8c3b7410b7d774267ef8967dc4386b8125
khush0032 <khush.c@aereo.io>
2025-02-12 03:27:52 +0530

    change frontend to Frontend

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '..', 'Frontend')));
 // console.log(path.join(__dirname, '..', 'Frontend'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+  res.sendFile(path.join(__dirname, '../Frontend', 'index.html'));
 });
 
 
